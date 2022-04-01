565fb43bfaaf5a6db4c8520a743961c5d4af00aa
saleebm <saleebmina@copt.dev>
2022-04-01 10:01:45 -0400

    more spam

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       script: 'main.js',
       instances: 1,
       exec_mode: 'fork',
-      cron_restart: '0,30 * * * *',
+      cron_restart: '0,15 * * * *',
       watch: false,
       autorestart: false
     }
